60167c753db255aa3fbdf19dd9c269509b21ebb0
Harshil Shah <shah.harshil2@northeastern.edu>
2023-08-12 17:55:42 -0400

    update model

--- src/main/java/edu/neu/csye7374/Models/Room.java
+++ src/main/java/edu/neu/csye7374/Models/Room.java
@@ -1,27 +1,46 @@
 package edu.neu.csye7374.Models;
 
+import java.util.List;
+
 /**
  * In a game there will be multiple rooms
  * Example: Theme xyz requires user to escape 3 rooms
  * Room 1 -> Room 2 -> Room 3 -> Escape (in that order)
  * 
- * Using builder pattern to create a room
+ * Abstract class to represent a room
  */
-public class Room {
-    int id;
-    String name;
-    Puzzle[] puzzles;
 
-    public Room(int id, String name, Puzzle[] puzzles) {
+ public abstract class Room {
+    int id;
+    String name; // name of the room
+    String description; // description of the room
+    List<Puzzle> puzzles; // puzzles in the room
+    Puzzle exitPuzzle; // id of the puzzle that will lead to the next room // null if no next room
+    Room nextRoom; // ids of the next rooms // null if no next room
+
+    public Room(int id, String name, String description, List<Puzzle> puzzles, Room nextRoom, Puzzle exitPuzzle) {
         this.id = id;
         this.name = name;
+        this.description = description;
         this.puzzles = puzzles;
+        this.nextRoom = nextRoom;
+        this.exitPuzzle = exitPuzzle;
     }
 
-    public static class RoomBuilder {
+    abstract public void enterRoom();
+    abstract public void exitRoom();
+    abstract public void playRoom();
+
+    /**
+     * Since room requires too many parameters, we are using builder pattern to create a room
+     */
+    public abstract static class RoomBuilder {
         int id;
         String name;
-        Puzzle[] puzzles;
+        String description;
+        List<Puzzle> puzzles;
+        Puzzle exitPuzzle = null; //default value
+        Room nextRoom = null; //default value
 
         public RoomBuilder setId(int id) {
             this.id = id;
@@ -33,13 +52,28 @@ public class Room {
             return this;
         }
 
-        public RoomBuilder setPuzzles(Puzzle[] puzzles) {
+        public RoomBuilder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public RoomBuilder setPuzzles(List<Puzzle> puzzles) {
             this.puzzles = puzzles;
             return this;
         }
 
-        public Room build() {
-            return new Room(id, name, puzzles);
+        public RoomBuilder setNextRoom(Room room) {
+            this.nextRoom = room;
+            return this;
         }
+
+        public RoomBuilder setExitPuzzle(Puzzle puzzle) {
+            this.exitPuzzle = puzzle;
+            return this;
+        }
+
+        abstract public Room build();
     }
-}
+   
+
+ }
