ea0fd002ab010a21e645a819c9f24d1b4bf23e41
ApoorvaGururajK <kulkarni.apoo@northeastern.edu>
2023-08-13 15:32:14 -0400

    Created the UserProfile class and used Builder battern to build the profile

--- src/main/java/edu/neu/csye7374/Models/UserProfile.java
+++ src/main/java/edu/neu/csye7374/Models/UserProfile.java
@@ -1,4 +1,51 @@
 package edu.neu.csye7374.Models;
 
+/**
+ * Base class to represent a Player's profile
+ * Implemented Builder design pattern to build the Player's profile
+ */
 public class UserProfile {
+    int id;
+    String userName;
+    String email;
+
+    public UserProfile(int id, String userName, String email) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "ID='" + this.id + '\'' +
+                ", UserName='" + this.userName + '\'' +
+                ", Email='" + this.email + '\'';
+    }
+
+    public static class UserProfileBuilder {
+        int id;
+        String userName;
+        String email;
+
+        public UserProfileBuilder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserProfileBuilder setUserName(String userName) {
+            this.userName = userName;
+            return this;
+        }
+
+        public UserProfileBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserProfile build() {
+            return new UserProfile(id, userName, email);
+        }
+    }
 }
+
+
