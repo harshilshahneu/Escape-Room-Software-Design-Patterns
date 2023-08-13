fd252c2116fca8176c8cde343485b0630993381b
Harshil Shah <shah.harshil2@northeastern.edu>
2023-08-13 17:00:39 -0400

    Adding Singleton Factory to create a room

---
+++ src/main/java/edu/neu/csye7374/Models/RoomFactory.java
@@ -0,0 +1,5 @@
+package edu.neu.csye7374.Models;
+
+public abstract class RoomFactory {
+    public abstract Room createRoom();
+}
---
+++ src/main/java/edu/neu/csye7374/Rooms/Factories/EagerSingletonJailBreakFactory.java
@@ -0,0 +1,27 @@
+package edu.neu.csye7374.Rooms.Factories;
+
+import edu.neu.csye7374.Models.Room;
+import edu.neu.csye7374.Models.RoomFactory;
+import edu.neu.csye7374.Rooms.JailBreak;
+
+/**
+ * Example Usage : Room jailbreak = (Room) EagerSingletonJailBreakFactory.getInstance().createRoom();
+ */
+
+public class EagerSingletonJailBreakFactory extends RoomFactory{
+    private static EagerSingletonJailBreakFactory instance = new EagerSingletonJailBreakFactory();
+
+    private EagerSingletonJailBreakFactory() {
+        // private constructor to prevent instantiation
+    }
+
+    public static EagerSingletonJailBreakFactory getInstance() {
+        return instance;
+    }
+
+    @Override
+    public Room createRoom() {
+        return new JailBreak.JailBreakBuilder().build();
+    }
+    
+}
